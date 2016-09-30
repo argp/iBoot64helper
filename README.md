eae98e8e06b1532b743e13bdfa54a32cc07fe4bb
Patroklos Argyroudis <argp@census-labs.com>
2016-09-30 14:27:29 +0300

    First public release

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # iBoot64helper
-IDAPython utility to help with iBoot64 reverse engineering
+
+## Introduction
+
+This aims to become an IDAPython utility to help with iBoot64 reverse
+engineering. Currently it just locates iBoot's proper loading address,
+rebases the image, and identifies ARM64 functions based on a common
+function prologue. As you can see in the screenshot below, around 790
+functions are recognized after running it.
+
+<p align="center"><img src="screenshot.png"/></p>
+
+It's not much at this point, but hopefully it can help you start reversing
+the beast ;)
+
+I will be adding features to it, like function renaming based on string
+usage, etc.
+
+argp@census-labs.com, Fri Sep 30 13:50:29 EEST 2016
+
+## References
+[iOS RE Wiki](https://github.com/kpwn/iOSRE/blob/master/wiki/iBoot-RE.md)
---
+++ iBoot64helper.py
@@ -0,0 +1,54 @@
+# argp@census-labs.com, Fri Sep 30 13:50:29 EEST 2016
+
+import idautils
+import idaapi
+import idc
+
+def main():
+    base_addr = 0
+    ea = 0
+    idc.MakeFunction(ea)
+
+    # heuristic
+    while(true):
+        mnemonic = idc.GetMnem(ea)
+        
+        if "LDR" in mnemonic:
+            base_str = idc.GetOpnd(ea, 1)
+            base_addr = int(base_str.split("=")[1], 16)
+            
+            break
+
+        ea += 4
+
+    print("[+] rebasing to address 0x%x" % (base_addr))
+    idc.rebase_program(base_addr, idc.MSF_FIXONCE)
+    idaapi.autoWait()
+
+    segment_start = base_addr
+    segment_end = idc.GetSegmentAttr(segment_start, idc.SEGATTR_END)
+
+    ea = segment_start
+
+    print("[+] searching and defining functions")
+
+    while ea != idc.BADADDR:
+        ea = idc.FindBinary(ea, idc.SEARCH_DOWN, "BF A9", 16)
+            
+        if ea != idc.BADADDR:
+            ea = ea - 2
+
+            if (ea % 4) == 0 and idc.GetFlags(ea) < 0x200:
+                # print("[+] defining a function at 0x%x" % (ea))
+                idc.MakeFunction(ea)
+
+            ea = ea + 4
+            
+    idc.AnalyzeArea(segment_start, segment_end)
+    idaapi.autoWait()
+
+if __name__ == "__main__":
+    main()
+    print("[+] iboot64helper finished")
+
+# EOF
