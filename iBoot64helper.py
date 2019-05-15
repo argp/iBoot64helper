28714b47fa9f9e7033d52674990be57cecddd734
Patroklos Argyroudis <argp@census-labs.com>
2019-05-15 13:30:04 +0300

    Refactored to loader

--- iBoot64helper.py
+++ iBoot64helper.py
@@ -1,17 +1,55 @@
-# argp@census-labs.com, Fri Sep 30 13:50:29 EEST 2016
+# argp@census-labs.com, Wed 15 May 2019 01:05:33 PM EEST
 
 import idautils
 import idaapi
 import idc
+import struct
 
 true = True
 false = False
 none = None
 
-def main():
+def accept_file(fd, fname):
+    version = 0
+    ret = 0
+
+    if type(fname) == str:
+        fd.seek(0x280)
+        ver_str = fd.read(0x20)
+
+        if ver_str[:5] == "iBoot":
+            version = ver_str[6:] # for later
+            ret = {"format" : "iBoot (AArch64)", "processor" : "arm"}
+
+    return ret
+
+def load_file(fd, neflags, format):
+    size = 0
     base_addr = 0
     ea = 0
+
+    idaapi.set_processor_type("arm", idaapi.SETPROC_ALL)
+    idaapi.get_inf_structure().lflags |= idaapi.LFLG_64BIT
+    
+    if (neflags & idaapi.NEF_RELOAD) != 0:
+        return 1
+
+    fd.seek(0, idaapi.SEEK_END)
+    size = fd.tell()
+
+    segm = idaapi.segment_t()
+    segm.bitness = 2 # 64-bit
+    segm.start_ea = 0
+    segm.end_ea = size
+    idaapi.add_segm_ex(segm, "iBoot", "CODE", idaapi.ADDSEG_OR_DIE)
+
+    fd.seek(0)
+    fd.file2base(0, 0, size, false)
+
+    idaapi.add_entry(0, 0, "start", 1)
     idc.MakeFunction(ea)
+
+    print("[+] Marked as code")
 
     # heuristic
     while(true):
@@ -25,8 +63,8 @@ def main():
 
         ea += 4
 
-    print("[+] rebasing to address 0x%x" % (base_addr))
-    idc.rebase_program(base_addr, idc.MSF_FIXONCE)
+    print("[+] Rebasing to address 0x%x" % (base_addr))
+    idaapi.rebase_program(base_addr, idc.MSF_NOFIX)
     idaapi.autoWait()
 
     segment_start = base_addr
@@ -34,7 +72,7 @@ def main():
 
     ea = segment_start
 
-    print("[+] searching and defining functions")
+    print("[+] Searching and defining functions")
 
     while ea != idc.BADADDR:
         ea = idc.FindBinary(ea, idc.SEARCH_DOWN, "BF A9", 16)
@@ -43,7 +81,7 @@ def main():
             ea = ea - 2
 
             if (ea % 4) == 0 and idc.GetFlags(ea) < 0x200:
-                # print("[+] defining a function at 0x%x" % (ea))
+                # print("[+] Defining a function at 0x%x" % (ea))
                 idc.MakeFunction(ea)
 
             ea = ea + 4
@@ -51,8 +89,6 @@ def main():
     idc.AnalyzeArea(segment_start, segment_end)
     idaapi.autoWait()
 
-if __name__ == "__main__":
-    main()
-    print("[+] iboot64helper finished")
+    return 1
 
 # EOF
